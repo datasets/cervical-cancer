b3679c35390e1fc51023fc4ab62f4b0ce2f5a61c
svetozar <svetozar.cvele.stojkovic@gmail.com>
2018-01-04 10:44:45 +0100

    [main, csv][s]: missing value fix

--- scripts/main.py
+++ scripts/main.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 with open("../data/cervical-cancer.csv", "w") as bumps_file:
     for line in urlopen("https://archive.ics.uci.edu/ml/machine-learning-databases/00383/risk_factors_cervical_cancer.csv"):
         decodedLine = line.decode('UTF-8')
-        decodedLine = decodedLine.replace('?', 'N/A')
+        decodedLine = decodedLine.replace('?', '')
         bumps_file.write(decodedLine.strip() + '\n')
 
     bumps_file.close()
